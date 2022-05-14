bdd039c3af5e4c0c0d299b5b249fcac55326873c
Facundo Martinez <facundo.martinez@alumnos.uv.cl>
2022-05-14 17:42:35 -0400

    Actualizacion BD

--- tipecartagena.sql
+++ tipecartagena.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-05-2022 a las 01:32:22
+-- Tiempo de generación: 14-05-2022 a las 23:40:26
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.5
 
@@ -65,18 +65,19 @@ CREATE TABLE `archivos` (
 
 CREATE TABLE `categorias` (
   `id_categoria` int(11) NOT NULL,
-  `Rut` varchar(10) DEFAULT NULL,
-  `Categoria` enum('Artesania','Comida','Ropa y Accesorios','Decoracion','Muebleria') DEFAULT NULL,
-  `id_subcat` int(11) DEFAULT NULL
+  `Categoria` enum('Artesania','Comida','Ropa y Accesorios','Decoracion','Muebleria') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `categorias`
 --
 
-INSERT INTO `categorias` (`id_categoria`, `Rut`, `Categoria`, `id_subcat`) VALUES
-(2, '10243567-9', 'Ropa y Accesorios', NULL),
-(3, '13452388-6', 'Ropa y Accesorios', NULL);
+INSERT INTO `categorias` (`id_categoria`, `Categoria`) VALUES
+(1, 'Comida'),
+(2, 'Ropa y Accesorios'),
+(3, 'Decoracion'),
+(4, 'Artesania'),
+(5, 'Muebleria');
 
 -- --------------------------------------------------------
 
@@ -90,9 +91,16 @@ CREATE TABLE `direccion` (
   `numero` int(11) NOT NULL,
   `CasaDepto` enum('Casa','Departamento','','') DEFAULT NULL,
   `localidad` text NOT NULL,
-  `PoblaVilla` text DEFAULT NULL,
-  `Rut` varchar(10) NOT NULL
+  `PoblaVilla` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `direccion`
+--
+
+INSERT INTO `direccion` (`id_direccion`, `calle`, `numero`, `CasaDepto`, `localidad`, `PoblaVilla`) VALUES
+(1, 'rosas', 111, 'Casa', 'cartagena', NULL),
+(2, 'trece norte', 210, 'Casa', 'Cartagena', 'Villa');
 
 -- --------------------------------------------------------
 
@@ -104,13 +112,13 @@ CREATE TABLE `formulario_solicitud` (
   `Nombre_pyme` varchar(100) NOT NULL,
   `Descripcion` varchar(1000) NOT NULL,
   `RSH` varchar(100) DEFAULT NULL,
-  `Medio_pago` varchar(100) DEFAULT NULL,
-  `Medio_entrega` varchar(100) DEFAULT NULL,
+  `Medio_pago` text NOT NULL,
+  `Medio_entrega` enum('Delivery','Retiro en tienda') DEFAULT NULL,
   `Empresa_registrada` enum('Si','No') NOT NULL,
   `Actividades_SII` varchar(100) DEFAULT NULL,
   `Patente_permiso` varchar(100) DEFAULT NULL,
   `R_sanitaria` varchar(100) DEFAULT NULL,
-  `estado_sol` enum('En espera','Aprobado','Denegado','Modificacion') DEFAULT NULL,
+  `estado_sol` enum('En espera','Aprobado','Denegado','Modificacion') DEFAULT 'En espera',
   `Sitio_web` varchar(200) DEFAULT NULL,
   `Facebook` varchar(100) DEFAULT NULL,
   `Whatsapp` varchar(100) DEFAULT NULL,
@@ -128,8 +136,9 @@ CREATE TABLE `formulario_solicitud` (
 --
 
 INSERT INTO `formulario_solicitud` (`Nombre_pyme`, `Descripcion`, `RSH`, `Medio_pago`, `Medio_entrega`, `Empresa_registrada`, `Actividades_SII`, `Patente_permiso`, `R_sanitaria`, `estado_sol`, `Sitio_web`, `Facebook`, `Whatsapp`, `Instagram`, `Comentario_admin`, `Rut`, `id_formulario`, `id_categoria`, `id_subcat`, `id_archivo`) VALUES
-('RopasLindas', 'Ropa linda', NULL, 'Efectivo', NULL, 'Si', NULL, NULL, NULL, 'Aprobado', NULL, NULL, NULL, NULL, NULL, '10243567-9', 1, 0, NULL, 0),
-('TecnologiasTenologicas', 'Tecnologia ventaaaas', NULL, NULL, NULL, 'No', NULL, NULL, NULL, 'Aprobado', NULL, NULL, NULL, NULL, NULL, '13452388-6', 2, 0, NULL, 0);
+('RopasLindas', 'Ropa linda', NULL, 'Efectivo', NULL, 'Si', NULL, NULL, NULL, 'Aprobado', NULL, NULL, NULL, NULL, NULL, '10243567-9', 1, 2, NULL, 0),
+('DecoHogarCartagena', 'Tecnologia ventaaaas', NULL, '', NULL, 'No', NULL, NULL, NULL, 'Aprobado', NULL, NULL, NULL, NULL, NULL, '13452388-6', 2, 3, NULL, 0),
+('MiArte', 'Arte', NULL, 'Efectivo', 'Delivery', 'Si', NULL, NULL, NULL, 'En espera', NULL, NULL, NULL, NULL, NULL, '10102312-9', 3, 4, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -143,6 +152,13 @@ CREATE TABLE `subcat` (
   `estado_subcat` enum('En espera','Aprobado','Denegado','Modificacion') NOT NULL DEFAULT 'En espera',
   `id_categoria` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `subcat`
+--
+
+INSERT INTO `subcat` (`id_subcat`, `nombre_subcat`, `estado_subcat`, `id_categoria`) VALUES
+(1, 'Pinturas', 'Aprobado', 4);
 
 -- --------------------------------------------------------
 
@@ -158,16 +174,19 @@ CREATE TABLE `usuario` (
   `Correo` varchar(100) NOT NULL,
   `Contrasena` varchar(16) NOT NULL,
   `Telefono` varchar(13) NOT NULL,
-  `id_direccion` int(11) NOT NULL
+  `id_direccion` int(11) NOT NULL,
+  `Rol` text NOT NULL DEFAULT 'User'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`Rut`, `Nombre`, `ApellidoP`, `ApellidoM`, `Correo`, `Contrasena`, `Telefono`, `id_direccion`) VALUES
-('10243567-9', 'Luis ', 'Prat', 'Rojas', 'ehdicwbcehuwb@hotmail.com', '12345', '987654321', 0),
-('13452388-6', 'Juan', 'Perez', 'Perez', 'perezperez1@gmail.com', '123458', '987654329', 0);
+INSERT INTO `usuario` (`Rut`, `Nombre`, `ApellidoP`, `ApellidoM`, `Correo`, `Contrasena`, `Telefono`, `id_direccion`, `Rol`) VALUES
+('10102312-9', 'Jose', 'Loyola', 'Hidalgo', 'jlh@gmail.com', '1313', '912121222', 1, 'User'),
+('10232134-5', 'Facundo', 'Martinez', 'Gulle', 'facundo.martinez@alumnos.uv.cl', '1010', '967691027', 2, 'Admin'),
+('10243567-9', 'Luis ', 'Prat', 'Rojas', 'ehdicwbcehuwb@hotmail.com', '12345', '987654321', 0, 'User'),
+('13452388-6', 'Juan', 'Perez', 'Perez', 'perezperez1@gmail.com', '123458', '987654329', 0, 'User');
 
 --
 -- Índices para tablas volcadas
@@ -189,8 +208,7 @@ ALTER TABLE `archivos`
 -- Indices de la tabla `categorias`
 --
 ALTER TABLE `categorias`
-  ADD PRIMARY KEY (`id_categoria`),
-  ADD KEY `Rut` (`Rut`);
+  ADD PRIMARY KEY (`id_categoria`);
 
 --
 -- Indices de la tabla `direccion`
@@ -228,28 +246,22 @@ ALTER TABLE `archivos`
   MODIFY `id_archivo` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `categorias`
---
-ALTER TABLE `categorias`
-  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
 -- AUTO_INCREMENT de la tabla `direccion`
 --
 ALTER TABLE `direccion`
-  MODIFY `id_direccion` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_direccion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `formulario_solicitud`
 --
 ALTER TABLE `formulario_solicitud`
-  MODIFY `id_formulario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_formulario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `subcat`
 --
 ALTER TABLE `subcat`
-  MODIFY `id_subcat` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_subcat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Restricciones para tablas volcadas
