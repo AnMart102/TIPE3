88694689faaeac724e16de017017385cc29ed878
FernandoVargas <fernando.vargas@alumnos.uv.cl>
2022-05-15 14:16:54 -0400

    Se muestran la info en indexjs pero falta arreglar la consulta para las nuevas tablas revisar ultimo sql subido cambios en id_formularios que conectan con categorias archivo y categorias con sub cat !

--- src/controllers/f5_index.js
+++ src/controllers/f5_index.js
@@ -5,18 +5,22 @@ const router = express.Router();
 const conn = require('../database');
 
 exports.upgrade = (req, res)=>{
-    conn.query('SELECT Nombre_pyme,Descripcion,Categoria FROM formulario_solicitud AS f JOIN categorias as c WHERE f.id_categoria=c.id_categoria', (error,results) =>{
+    
+    conn.query('SELECT Nombre_pyme,Descripcion FROM formulario_solicitud ', (error,results) =>{
         if(error){
             throw error;
         }else{
-            res.send(results);
+            datos1=results;
+            console.log(datos1);
         }
     })
-    conn.query('Select DISTINCT Categoria FROM categorias', (error,results) =>{
+    conn.query('Select DISTINCT categorias FROM categorias', (error,results) =>{
         if(error){
             throw error;
         }else{
-            res.send(results);
+            datos2=results;
+            console.log(datos2);
         }
     })
+    res.render("index.ejs")
 }   
